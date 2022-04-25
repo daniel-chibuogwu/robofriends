c3cddff5c4abde4750ac7ac5d76f7bb1618d9168
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2022-04-25 22:31:42 +0100

    Added response for robot not found

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,21 +2,22 @@ import Card from "./Card";
 import React from "react";
 
 const CardList = ({robots}) => {
-
-    return (
-        <div className='tc'>
-            {
-                robots.map((user, i) => {
-                    return (
-                        <Card
-                            key={i}
-                            id={robots[i].id}
-                            name={robots[i].name}
-                            email={robots[i].email}
-                        />);
-                })
-            }
-        </div>
-    );
+        return (!robots.length) ?
+        <h1 className='f3 light-red'>Ooops!!! Robot  not found 🙁</h1> :
+       (
+            <div className='tc'>
+                {
+                    robots.map((user, i) => {
+                        return (
+                            <Card
+                                key={i}
+                                id={robots[i].id}
+                                name={robots[i].name}
+                                email={robots[i].email}
+                            />);
+                    })
+                }
+            </div>
+        );
 }
 export default CardList;
