3872ac6f04b2acb038e952bb17f77b946a9c6600
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2022-04-25 17:09:43 +0100

    first commit

---
+++ src/components/Scroll.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        <div style={{
+            overflow: 'scroll',
+            height: '900px',
+            'border-top': '1px solid rgba(0, 128, 128, 1)',
+            width: '80%',
+            margin: '0 auto'
+        }}>
+            {props.children}
+        </div>
+    )
+}
+
+export default Scroll;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Daniel Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Paul Howell',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Seun Bauch',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Peace Lebsack',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chichi Dietrich',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+        id: 6,
+        name: 'Esther Schulist',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Mary Weissnat',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Emmanuel Runolfsdottir V',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me'
+    },
+    {
+        id: 9,
+        name: 'Dera Reichert',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Jason DuBuque',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz'
+    }
+];
