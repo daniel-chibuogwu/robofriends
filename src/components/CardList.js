7b806cf1bdaef62edcb9fdb18b5867e6ab7bc43b
dan-chibuogwu17 <danielchibuogwu@gmail.com>
2022-08-10 22:34:50 +0100

    fixed the error boundary for the card list component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,23 @@
 import Card from "./Card";
 import React from "react";
 
-const CardList = ({robots}) => {
-        return (!robots.length) ?
-        <h1 className='f3 light-red'>Ooops!!! Robot  not found 🙁</h1> :
-       (
-            <div className='tc'>
-                {
-                    robots.map((user, i) => {
-                        return (
-                            <Card
-                                key={i}
-                                id={robots[i].id}
-                                name={robots[i].name}
-                                email={robots[i].email}
-                            />);
-                    })
-                }
-            </div>
+const CardList = ({ robots }) => {
+  //   if (true) throw new Error("no!");
+  return !robots.length ? (
+    <h1 className="f3 light-red">Ooops!!! Robot not found 🙁</h1>
+  ) : (
+    <div className="tc">
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+          />
         );
-}
+      })}
+    </div>
+  );
+};
 export default CardList;
--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hasError: false
-        }
-    // }
-    // componentDidUpdate(error) {
-    //     this.setState({hasError: true});
-    }
-    render() {
-        if (this.state.hasError) {
-            return <h1>Ooops! Something went wrong</h1>
-        }
-        return this.props.children
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
 
+  render() {
+    if (this.state.hasError) {
+      return <h1>Ooops! Something went wrong</h1>;
+    }
+    return this.props.children;
+  }
 }
-
 
 export default ErrorBoundary;
